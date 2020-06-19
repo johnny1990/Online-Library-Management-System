e087c89372399621a26ece55e5a1cf3e5a1209bb
Man Ioan <man_ioan@live.com>
2020-06-19 17:21:51 +0300

    Add views for Clienti biblioteca

---
+++ Biblioteca/Controllers/ClientiController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
+
+namespace Biblioteca.Controllers
+{
+    public class ClientiController : Controller
+    {
+        private readonly BibliotecaDBContext _context;
+
+        public ClientiController(BibliotecaDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clienti
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clienti.ToListAsync());
+        }
+
+        // GET: Clienti/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clienti = await _context.Clienti
+                .FirstOrDefaultAsync(m => m.IdClient == id);
+            if (clienti == null)
+            {
+                return NotFound();
+            }
+
+            return View(clienti);
+        }
+
+        // GET: Clienti/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clienti/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdClient,Nume,Adresa,Contact")] Clienti clienti)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(clienti);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(clienti);
+        }
+
+        // GET: Clienti/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clienti = await _context.Clienti.FindAsync(id);
+            if (clienti == null)
+            {
+                return NotFound();
+            }
+            return View(clienti);
+        }
+
+        // POST: Clienti/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdClient,Nume,Adresa,Contact")] Clienti clienti)
+        {
+            if (id != clienti.IdClient)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(clienti);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientiExists(clienti.IdClient))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(clienti);
+        }
+
+        // GET: Clienti/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clienti = await _context.Clienti
+                .FirstOrDefaultAsync(m => m.IdClient == id);
+            if (clienti == null)
+            {
+                return NotFound();
+            }
+
+            return View(clienti);
+        }
+
+        // POST: Clienti/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var clienti = await _context.Clienti.FindAsync(id);
+            _context.Clienti.Remove(clienti);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientiExists(int id)
+        {
+            return _context.Clienti.Any(e => e.IdClient == id);
+        }
+    }
+}
