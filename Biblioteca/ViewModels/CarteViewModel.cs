97547fe678beae8069a96e68b4882ad30f1d74a9
Man Ioan <man_ioan@live.com>
2020-06-20 15:55:55 +0300

    Add vm

---
+++ Biblioteca/ViewModels/CarteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.ViewModels
+{
+    public class CarteViewModel
+    {
+        public int IdCarte { get; set; }
+
+        public string Titlu { get; set; }
+
+        public string Cod { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Editura { get; set; }
+
+        public bool Disponibil { get; set; }
+    }
+}
